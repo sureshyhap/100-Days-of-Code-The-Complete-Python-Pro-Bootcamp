a5f4ca7bd15c96d4af7ad8e3763014f77e534a29
Suresh Yhap <sureshyhap@gmail.com>
2022-06-06 17:13:41 -0400

    Finished Day 5

---
+++ Day 5/adding_even_numbers.py
@@ -0,0 +1,5 @@
+#Write your code below this row 👇
+sum_even = 0
+for num in range(2, 101, 2):
+    sum_even += num
+print(sum_even)
---
+++ Day 5/average_height.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+sum = 0
+num = 0
+for height in student_heights:
+    sum += height
+    num += 1
+average = sum / num
+print(round(average))
