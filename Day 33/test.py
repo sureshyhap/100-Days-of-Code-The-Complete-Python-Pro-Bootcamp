7c0e27ee3c0a04047444ab01d454d9bf908f81c5
Suresh Yhap <sureshyhap@gmail.com>
2022-09-12 16:57:26 -0400

    Added some API parameters to Endpoint GET request

--- Day 33/test.py
+++ Day 33/test.py
@@ -1,6 +1,38 @@
+"""
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 
 data = response.json()
+print(data)
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+print(iss_position)
+"""
+
+import requests
+import datetime as dt
+
+LATITUDE = 40.715160,
+LONGITUDE = -73.760270
+
+parameters = {
+    "lat" : LATITUDE,
+    "lng" : LONGITUDE,
+    "formatted" : 0
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+
+data = response.json()
+sunrise_time = data["results"]["sunrise"]
+sunset_time = data["results"]["sunset"]
+
+print(sunrise_time)
+print(sunset_time)
+
+now = dt.datetime.now()
